60d61676eceeaf72b8241a899133bb1a6ffe36b2
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2024-10-18 22:16:38 +0530

    connection to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
- import mongoose from 'mongoose'
+import dotenv from 'dotenv';
 
-function connectDB(){}
+
+import connectDB from './db/db.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+/* first approach to connect to database using IIFE(Immediately Invoked Function Expression)
+
+import express from 'express'
+const app = express();
+
+( async () => {
+    try {
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        app.on('error', (error)=>{
+            console.log('ERROR: ',error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        });
+        
+    } catch (error) {
+        console.error('ERROR: ',error);
+        throw error;
+    }
+})()*/
