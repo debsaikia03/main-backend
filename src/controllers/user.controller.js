bb38667cb21c1fb542f266347936c9cca979d9db
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2024-12-16 21:44:59 +0530

    add getWatchHistory function and update user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { apiResponse } from '../utils/apiResponse.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 
 const generateAccessAndRefreshTokens = async(userId) =>
@@ -453,12 +454,65 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
     if(!channel?.length){  //check for channel
         throw new apiError(404, "Channel not found");
-    }
+    };
     
     return res
     .status(200)
     .json(
         new apiResponse(200, channel[0], "user channel profile fetched successfully")
+    );
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+
+    const user = await User.aggregate([
+        {
+
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup : {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addfields : {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200, user[0].watchHistory, "Watch history fetched successfully")
     );
 });
 
@@ -472,6 +526,8 @@ export {
     getCurrrentUser, 
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 
 };
